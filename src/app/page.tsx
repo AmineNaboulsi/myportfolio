9a2c95f4adf8ec1a118a8367a8d76ca906939a30
Naboulsi Amine <140864712+AmineNaboulsi@users.noreply.github.com>
2025-05-31 19:36:43 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -211,10 +211,7 @@ export default function Page() {
                 Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a dm{" "}
-                
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                Want to chat? Just shoot me a dm with a direct question on linkedin and I&apos;ll respond whenever I can. I will ignore all soliciting.
               </p>
             </div>
           </BlurFade>
